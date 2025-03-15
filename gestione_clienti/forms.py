8d4875e643b252312997addaa59ac9d7766d28e6
fabio <fabio.grossi84@gmail.com>
2025-03-15 23:08:38 +0100

    Modifico il form di gestione clienti per assegnare un freelance obbligatorio

--- gestione_clienti/forms.py
+++ gestione_clienti/forms.py
@@ -1,7 +1,26 @@
 from django import forms
 from .models import Cliente
+from gestione_utenti.models import User
 
 class ClienteForm(forms.ModelForm):
+    
+    freelance = forms.ModelChoiceField(
+        queryset=User.objects.filter(ruolo="F"),  # Mostra solo Freelance
+        empty_label="Seleziona un Freelance",
+        required=True  # Freelance obbligatorio
+    )
+    
+    
     class Meta:
         model = Cliente
         fields = ["nome", "email", "azienda", "telefono_fisso", "cellulare"]
+    
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)  # Ottengo l'utente che sta creando il Cliente
+        super().__init__(*args, **kwargs)
+
+        # Se l'utente è un Freelance, assegno automaticamente il campo e lo nascondo
+        # Se l'utente è un Admin, mostro il campo Freelance
+        if user and user.is_freelance():
+            self.fields["freelance"].initial = user
+            self.fields["freelance"].widget = forms.HiddenInput()

c157254812678109fe0d3365e57a718067e64da4
fabio <fabio.grossi84@gmail.com>
2025-03-17 01:36:30 +0100

    Aggiunte foto, sistemato base e rifatto home.

--- freelancer_manager_pro/settings.py
+++ freelancer_manager_pro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4#ef$j)o9o8+_nm&u2qj1@=k5@9sx%k$wq&@@*ev&x%xf)(+ho'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False # Solo con DEBUG=False Django mostra 404 custom
+DEBUG = True # Solo con DEBUG=False Django mostra 404 custom
 
 ALLOWED_HOSTS = ['*'] # Per test locale, usa '*' o ['127.0.0.1', 'localhost']
 
